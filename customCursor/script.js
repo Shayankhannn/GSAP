d5a7fb968ccbb3703cdb2d4d99c19af34317483d
shayankhannn <shayannkhannn007@gmail.com>
2025-01-09 13:48:18 +0500

    custom cursor done

--- customCursor/script.js
+++ customCursor/script.js
@@ -3,8 +3,32 @@
 
 var main = document.querySelector("#main")
 var cursor = document.querySelector("#cursor")
+var image = document.querySelector("#image")
 
-// dets is just a example
-main.addEventListener("mousemove", function(dets){
 
+main.addEventListener("mousemove", function(e){
+
+    gsap.to(cursor,{
+        x:e.x,
+        y:e.y,
+        ease:"back.Out"
+    })
+
+
+})
+
+image.addEventListener("mouseenter", function(e){
+    cursor.innerHTML = "View"
+    // cursor.style.backgroundImage = "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
+    gsap.to(cursor,{
+        scale:4,
+        backgroundImage:"https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" 
+    })
+})
+image.addEventListener("mouseleave", function(e){
+    cursor.innerHTML = ""
+    gsap.to(cursor,{
+        scale:1,
+       
+    })
 })
